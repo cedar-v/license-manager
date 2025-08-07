3f291cc363b963ba94e813441aaa0589984472c0
13895237362 <2205451508@qq.com>
2025-08-07 09:42:46 +0800

    fix:改变i8n和element-plus引入方式

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,7 @@
  * @Author: 13895237362 2205451508@qq.com
  * @Date: 2025-07-29 09:37:26
  * @LastEditors: 13895237362 2205451508@qq.com
- * @LastEditTime: 2025-08-04 16:55:57
+ * @LastEditTime: 2025-08-05 15:17:46
  * @FilePath: /vue-demo3.0/src/main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,15 +13,17 @@ import router from './router'
 // 全局样式重置
 import './assets/styles/global.css'
 
-import i18n from './i18n' // 国际化语言配置
-import { loadLocaleMessages,SupportedLocale } from './i18n'
-// 加载默认语言包
-loadLocaleMessages(i18n.global.locale.value as SupportedLocale)
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import i18n, { getCurrentElementLocale } from './i18n'
 
-
-import 'amfe-flexible' // 引入 flexible,实现全端适配
 
 const app = createApp(App)
-app.use(router)
+
+// 正确配置Element Plus语言包
+app.use(ElementPlus, {
+  locale: getCurrentElementLocale()
+})
 app.use(i18n)
+app.use(router)
 app.mount('#app')
