af382db83f946aaddfc6ba10ae096460bb34ee2d
13895237362 <2205451508@qq.com>
2025-08-07 09:41:18 +0800

    fix: 修复多语言切换,全局不改变

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,34 @@
+<!--
+ * @Author: 13895237362 2205451508@qq.com
+ * @Date: 2025-08-05 14:46:50
+ * @LastEditors: 13895237362 2205451508@qq.com
+ * @LastEditTime: 2025-08-05 15:31:22
+ * @FilePath: /frontend/src/App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
-  <div id="app">
+  <el-config-provider :locale="elementLocale">
     <router-view />
-  </div>
+  </el-config-provider>
 </template>
 
 <script setup lang="ts">
-// 根组件，只负责渲染路由视图
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import en from 'element-plus/es/locale/lang/en'
+import ja from 'element-plus/es/locale/lang/ja'
+
+const { locale } = useI18n()
+
+ const localeMap = {
+    zh: zhCn,
+    en: en,
+    ja: ja
+  }
+const elementLocale = computed(() => {
+  return localeMap[locale.value as keyof typeof localeMap] || zhCn
+})
 </script>
 
 <style>
